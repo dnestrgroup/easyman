9704e1f37c7c849d02164f2b4700e0421882ef4e
Volodymyr <47746321+dnestrgroup@users.noreply.github.com>
2023-06-20 11:13:46 +0300

    Add files via upload

---
+++ resources/views/newuser-registered.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+ <title> Registration successful </title>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta http-equiv="Content-Language" content="ru" />
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            
+            <meta name="keywords" content="Registration successful">
+            <meta name="description" content="New User created successfully">
+
+            <link rel="icon" href="../public/images/favicon.ico" type="image/x-icon">
+            <link rel="shortcut icon" href="../public/images/favicon.ico" type="image/x-icon">
+
+                <link rel="stylesheet" type="text/css" href="../public/css/newuser/style-registration-body.css" />
+                <link rel="stylesheet" type="text/css" href="../public/css/newuser/style-registration-fonts.css" />
+                <link rel="stylesheet" type="text/css" href="../public/css/newuser/style-registration-links.css" />
+                <link rel="stylesheet" type="text/css" href="../public/css/newuser/style-registration-objects.css" />
+
+</head>
+<body>
+
+
+
+<!-- =========== HEADER WHITE LONG ===========  -->
+<div id="header">
+    <div class="wrapper">
+
+        <div class="header__left-part">
+            <div class="header__left-banner_align">
+                <font class="header__logo-black"> Easyman // </font>
+                <font class="header__logo-blue"> Registration </font>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+<!-- =========== MENU BLOCK LIGHT-GREY ===========  -->
+<div id="menublock">
+    <div class="wrapper">
+
+<br>
+<br>
+
+<!-- Message window - that new room created -->
+<div class="message_successful">
+ <b> New User created successfully ! </b> <br>
+</div>
+
+        <div class="menublock__spacer"> </div>
+        <a href="/#anchor_reception"> Go to Reception >> </a>
+        <div class="menublock__spacer"> </div>
+
+    </div>
+</div>
+
+
+
+<!-- =========== FOOTER ===========  -->
+<div id="footer">
+    <div class="wrapper">
+
+        <div class="footer__part2-shortinfo">
+            <div class="footer__info">
+                Our application continually strives for excellence, so if you find bugs or if you get trouble using our app, please email us.
+                <br><br>
+                Partnership, support, question and help only by mail
+                <font style="font-family: Verdana, Arial; font-size: 0.8em; color: #17dfd2;"> dnestrgroup@gmail.com </font>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+</body>
+</html>
